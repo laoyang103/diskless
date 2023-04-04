26797c5d53c8c3c367f9a8db594c363a8e0239b4
laoyang103 <553069938@qq.com>
2023-04-04 01:32:28 +0000

    基本可用但IP写死为27

---
+++ install.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# 安装所需要软件包
+yum -y install dhcp xinetd tftp-server vsftpd wget 
+
+# 配置DHCP网络参数
+cat > /etc/dhcp/dhcpd.conf << EOF
+default-lease-time 600;
+max-lease-time 7200;
+subnet 10.10.10.0 netmask 255.255.255.0 {
+  range 10.10.10.100 10.10.10.200;
+  option routers 10.10.10.254;
+  option domain-name-servers 114.114.114.114;
+  next-server 10.10.10.27;
+  filename "lpxelinux.0";
+}
+EOF
+read -p "请按任意键继续"
+
+# 替换tftp配置文件中disable=no
+sed -i 's/disable\t\t\t= yes/disable\t\t\t= no/g' /etc/xinetd.d/tftp
+
+tftpPath="/var/lib/tftpboot"
+# 下载启动引导程序，解压到tftp目录
+wget http://192.168.10.254:8080/jxfiles/syslinux.tar.gz
+tar -zxf syslinux.tar.gz -C $tftpPath
+mv $tftpPath/syslinux604files/* $tftpPath
+read -p "请按任意键继续"
+
+# 下载win8pe镜像到/var/ftp/pub目录
+wget http://192.168.10.254:8080/iso/winpe/win8pe.iso
+mv win8pe.iso /var/ftp/pub
+read -p "请按任意键继续"
+
+# 创建启动菜单文件
+mkdir -p $tftpPath/pxelinux.cfg
+cat > $tftpPath/pxelinux.cfg/default << EOF
+DEFAULT menu.c32
+TIMEOUT 30
+
+LABEL win8pe
+  KERNEL memdisk
+  APPEND initrd=ftp://\${next-server}/pub/win8pe.iso iso raw
+EOF
+read -p "请按任意键继续"
+
+# 启动各种服务，关闭防火墙和selinux
+systemctl restart dhcpd
+systemctl restart xinetd
+systemctl restart vsftpd
+systemctl stop firewalld
+setenforce 0
